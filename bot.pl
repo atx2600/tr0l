1f3fadb67f6853a22077e3f084f7aeaacf86aaae
arrdem <rmckenzie92@gmail.com>
2013-04-12 05:58:11 -0500

    add the other active .pl plugins

--- bot.pl
+++ bot.pl
@@ -1,30 +1,42 @@
+# == WHAT
+# All around bot.
+#
+# == WHO
+# Jeroen Van den Bossche, 2012
+#
+# == INSTALL
+# Save it in ~/.irssi/scripts/ and do /script load bot.pl
+# OR
+# Save it in ~/.irssi/scripts/autorun and (re)start Irssi
+
 use strict;
 use Irssi;
 use LWP::Simple;
 use HTML::TokeParser;
 use vars qw($VERSION %IRSSI);
 
-$VERSION = '0.1.0';
+$VERSION = '0.1';
 %IRSSI = (
-    authors => 'Reid McKenzie',
-    name => 'Bot',
-    description => 'All around Irssi bot.',
-    license => 'GPL',
+	authors => 'Jeroen Van den Bossche',
+	name => 'bot',
+	description => 'All around Irssi bot.',
+	license => 'GPL',
+	url => 'http://vanbosse.be',
 );
 
 $Bot::KARMA = ( );
 %Bot::IS = ( );
 
 sub process_message {
-    my ($server, $msg, $target, $nick) = @_;
-    my @command = 0;
-    my ($cmd, $m1, $m2, $m3) = 0;
+	my ($server, $msg, $target, $nick) = @_;
+	my @command = 0;
+	my ($cmd, $m1, $m2, $m3) = 0;
     return unless $target =~ /^#(atx2600)/;
     @command = split(' ', $msg);
     if(@command[0] =~ /arrdem-lurkin:?/) {
         shift(@command);
     }
-    $cmd = @command[0];
+  	$cmd = @command[0];
     shift(@command);
     $m1 = @command[0];
     $m2 = @command[1];
@@ -38,14 +50,14 @@ sub process_message {
 #    Irssi::print("\$1        " . $m1);
 #    Irssi::print("\$2        " . $m2);
 #    Irssi::print("\$3        " . $m3);
-
+	
     return unless $cmd;
-    return if $msg eq $cmd;
+	return if $msg eq $cmd;
 
-    my $output = "";
-    if ($cmd eq "help")
-    {
-        $output = "Commands: "
+	my $output = "";
+	if ($cmd eq "help")
+	{
+		$output = "Commands: "
                  ."!slap <user>, "
                  ."!inc <user>, "
                  ."!dec <user>, "
@@ -53,11 +65,11 @@ sub process_message {
                  ;
                  #"!alias <user> <alias>, ".
                  #"!aliases <user>";
-    }
-    elsif ( $nick ne $1
+	}
+	elsif ( $nick ne $1 
             and not exists({map { $_ => 1 } $Bot::IS{$nick}}->{$nick})) {
         if ($cmd eq "!inc")
-        {
+	    {
             my ($n) = 0;
             $n = 1 + $Bot::KARMA{$m1};
             $Bot::KARMA{$m1} = $n;
@@ -68,10 +80,10 @@ sub process_message {
             my ($n) = 0;
             $n = $Bot::KARMA{$m1} - 1;
             $Bot::KARMA{$m1} = $n;
-            $output = "$m1 -> $n";
+            $output = "$m1 -> $n";   
         }
         elsif($cmd eq "!karma")
-        {
+        {   
             my ($n) = 0;
             $n = $Bot::KARMA{$m1} | 0;
 
@@ -86,10 +98,10 @@ sub process_message {
                 $output = $output . "s";
             }
         }
-     elsif ($cmd eq "!slap" && $1)
-        {
-            $output = "$1 got slapped around a bit with a large trout.";
-        }
+    	elsif ($cmd eq "!slap" && $1)
+	    {
+    		$output = "$1 got slapped around a bit with a large trout.";
+    	}
     }
     elsif ( $cmd eq "!alias" )
     {
@@ -101,17 +113,17 @@ sub process_message {
         $output = "$m1 is also known as $Bot::IS{$m1}.";
     }
 
-    $server->command("msg $target $output");
+	$server->command("msg $target $output");
 }
 
 Irssi::signal_add_last('message public', sub {
-    my ($server, $msg, $nick, $mask, $target) = @_;
-    Irssi::signal_continue($server, $msg, $nick, $mask, $target);
-    process_message($server, $msg, $target, $nick);
+	my ($server, $msg, $nick, $mask, $target) = @_;
+	Irssi::signal_continue($server, $msg, $nick, $mask, $target);
+	process_message($server, $msg, $target, $nick);
 });
 
 Irssi::signal_add_last('message own_public', sub {
-    my ($server, $msg, $target) = @_;
-    Irssi::signal_continue($server, $msg, $target);
-    process_message($server, $msg, $target);
+	my ($server, $msg, $target) = @_;
+	Irssi::signal_continue($server, $msg, $target);
+	process_message($server, $msg, $target);
 });
