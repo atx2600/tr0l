3f7f33ce96e5fa3fd4ae9dbaa1e2ba088b10e9ef
arrdem <rmckenzie92@gmail.com>
2013-04-28 03:40:32 -0500

    move the core commands out into their own file

---
+++ tr0l/core.pl
@@ -0,0 +1,30 @@
+package tr0l;
+use strict;
+use warnings;
+
+command_set_default("_DEFAULT_",
+                    sub{
+                      my ($chan, $nick, @args) = @_;
+                        return "msg $chan no intiendo.";
+                    });
+
+command_set_handler("!help", "Prints this help message",
+                    sub {
+                      my ($chan, $nick, @args) = @_;
+                        return "msg $chan Commands:\n  " .
+                            join(",\n  ", map("$_ : $HELP{$_}",
+                                            keys %HELP));
+                    });
+
+command_set_handler("!slap", "!slap <user> slaps a ho",
+                    sub{
+                        my ($chan, $nick, @args) = @_;
+                        return "msg $chan ".
+                          "$args[0] got slapped around a bit with a large trout.";
+                    });
+
+command_set_handler("!version", "!version prints tr0l's version number.",
+                    sub{
+                      my ($chan, $nick, @args) = @_;
+                        return "msg $chan $VERSION";
+                    });
