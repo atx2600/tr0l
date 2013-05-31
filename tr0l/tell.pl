c49aecdc1fdbd01f314401bc2cb43a3bf154266d
arrdem <rmckenzie92@gmail.com>
2013-05-31 01:43:19 -0500

    friggin sigils

--- tr0l/tell.pl
+++ tr0l/tell.pl
@@ -13,16 +13,18 @@ command_set_handler("!tell", "!tell <user> [text], leave a message",
                         if (not defined($user)) {
                             return "msg $chan $nick !tell requires a nick argument";
                         }
-                        my (@msgs) = ($MSGS{$user} // ());
-                        @msgs = ((join(" ",(("$nick:"), @cmdargs))),@msgs);
-                        $MSGS{$user} = @msgs;
+                        my (@msgs) = (@MSGS{$user} // ());
+                        @msgs = ((join(" ",(("$nick:"),
+                                            @cmdargs))),
+                                 @msgs);
+                        @MSGS{$user} = @msgs;
                         return "msg $chan $nick: OK, I'll tell $user that!";
                     });
 
 command_set_handler("!mail", "!mail, get your messages in messages in PMs",
                     sub{
                         my ($chan, $nick, @cmdargs, $server) = @_;
-                        my (@msgs) = $MSGS{$nick};
-                        $MSGS{$nick} = ();
+                        my (@msgs) = @MSGS{$nick};
+                        @MSGS{$nick} = ();
                         return "msg $nick " . join("\n", @msgs);
                     });
