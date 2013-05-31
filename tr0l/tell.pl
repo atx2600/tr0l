af43f1702b433dbb2864f0d61f21ee746664ba97
arrdem <rmckenzie92@gmail.com>
2013-05-31 01:36:52 -0500

    patch for shift not pop, change @args to be @cmdargs for conflicting with @args global name

--- tr0l/tell.pl
+++ tr0l/tell.pl
@@ -8,20 +8,20 @@ our %MSGS = ("arrdem" => ());
 
 command_set_handler("!tell", "!tell <user> [text], leave a message",
                     sub{
-                        my ($chan, $nick, @args, $server) = @_;
-                        my ($user) = pop(@args);
+                        my ($chan, $nick, @cmdargs, $server) = @_;
+                        my ($user) = shift(@cmdargs);
                         if (not defined($user)) {
                             return "msg $chan $nick !tell requires a nick argument";
                         }
                         my (@msgs) = ($MSGS{$user} // ());
-                        @msgs = ((join(" ",(("$nick:"), @args))),@msgs);
+                        @msgs = ((join(" ",(("$nick:"), @cmdargs))),@msgs);
                         $MSGS{$user} = @msgs;
                         return "msg $chan $nick: OK, I'll tell $user that!";
                     });
 
 command_set_handler("!mail", "!mail, get your messages in messages in PMs",
                     sub{
-                        my ($chan, $nick, @args, $server) = @_;
+                        my ($chan, $nick, @cmdargs, $server) = @_;
                         foreach ($MSGS{$nick}) {
                             $server->command("msg $nick $_\n");
                         }
